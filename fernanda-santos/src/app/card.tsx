7257db90338fad007b3bc24601550a45b7dee13e
Estevão <estevaog.gomes@gmail.com>
2023-03-24 12:09:10 -0300

    [Feature]Finish carousel implementation
    Fixes carousel implementation by adding 'use client' directive on Social

--- fernanda-santos/src/app/card.tsx
+++ fernanda-santos/src/app/card.tsx
@@ -1,54 +1,56 @@
 import Image from "next/image";
-import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-interface cardProps{
-    name: string;
-    description: string;
-    source: string;
-    altText: string;
+interface cardProps {
+  name: string;
+  description: string;
+  source: string;
+  altText: string;
 }
 
-export function Card({name, description, source, altText}: cardProps){
-    return(
-        <div className={`w-9/12 lg:mb-0 mb-12 px-4`}>
-                <div className="px-6 flex">
-                  <Image 
-                    src={source}
-                    alt={altText}
-                    width={400}
-                    height={600}
-                    className="flex-1"
-                  />
-                  <div className="pt-6 ml-4 text-center flex-1">
-                    <h5 className="text-xl font-bold">
-                      {name}
-                    </h5>
-                    <p className="mt-1 text-sm text-gray-500 uppercase font-semibold">
-                      {description}
-                    </p>
-                    <div className="mt-6">
-                      <button
-                        className="bg-blue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-                        type="button"
-                      >
-                        <FontAwesomeIcon icon={faTwitter} />
-                      </button>
-                      <button
-                        className="bg-blue-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-                        type="button"
-                      >
-                        <FontAwesomeIcon icon={faFacebook} />
-                      </button>
-                      <button
-                        className="bg-pink-500 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-                        type="button"
-                      >
-                        <FontAwesomeIcon icon={faInstagram} />
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-    )
+export function Card({ name, description, source, altText }: cardProps) {
+  return (
+    <div className={`lg:mb-0 mb-12 px-2`}>
+      <div className="px-6 flex">
+        <Image
+          src={source}
+          alt={altText}
+          width={400}
+          height={600}
+          className="flex-1"
+        />
+        <div className="flex flex-col justify-center pt-6 ml-4 text-center flex-1">
+          <h5 className="text-xl font-bold">{name}</h5>
+          <p className="mt-1 text-sm text-gray-500 uppercase font-semibold">
+            {description}
+          </p>
+          <div className="mt-6">
+            <button
+              className="bg-blue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+              type="button"
+            >
+              <FontAwesomeIcon icon={faTwitter} />
+            </button>
+            <button
+              className="bg-blue-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+              type="button"
+            >
+              <FontAwesomeIcon icon={faFacebook} />
+            </button>
+            <button
+              className="bg-pink-500 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+              type="button"
+            >
+              <FontAwesomeIcon icon={faInstagram} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- fernanda-santos/src/app/social.tsx
+++ fernanda-santos/src/app/social.tsx
@@ -1,43 +1,43 @@
-import { Card } from "./card"
-import { Carousel } from 'react-responsive-carousel';
+"use client";
+import { Card } from "./card";
+import { Carousel } from "react-responsive-carousel";
 
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
-
-export function Social(){
-    return(
-        <section className="pt-20 pb-48">
-          <div className="mx-auto px-4">
-            <div className="flex flex-wrap justify-center text-center mb-24">
-              <div className="w-full lg:w-6/12 px-4">
-                <h2 className="text-4xl font-semibold">
-                  Veja alguns depoimentos
-                </h2>
-                <p className="text-lg leading-relaxed m-4 text-gray-600">
-                  Saiba um pouco do que nossos alunos tem a dizer sobre nossas aulas.
-                </p>
-              </div>
-            </div>
-            <Carousel autoPlay infiniteLoop interval={2000}>
-                <Card
-                  name="Interesting Person"
-                  description="Essa aula de inglês tem me ajudado imensamente. Minha pronunciação melhorou e consigo me comunicar mais facil e rapidamente."
-                  source="https://picsum.photos/800/600"
-                  altText="Placeholder Image"
-                />
-                <Card
-                  name="Interesting Person 2"
-                  description="Essa aula de inglês tem me ajudado imensamente. Minha pronunciação melhorou e consigo me comunicar mais facil e rapidamente."
-                  source="https://picsum.photos/800/600"
-                  altText="Placeholder Image"
-                />
-                <Card
-                  name="Interesting Person 3"
-                  description="Essa aula de inglês tem me ajudado imensamente. Minha pronunciação melhorou e consigo me comunicar mais facil e rapidamente."
-                  source="https://picsum.photos/800/600"
-                  altText="Placeholder Image"
-                />
-            </Carousel>
+export function Social() {
+  return (
+    <section className="pt-20 pb-48">
+      <div className="mx-auto px-4">
+        <div className="flex flex-wrap justify-center text-center mb-24">
+          <div className="w-full lg:w-6/12 px-4">
+            <h2 className="text-4xl font-semibold">Veja alguns depoimentos</h2>
+            <p className="text-lg leading-relaxed m-4 text-gray-600">
+              Saiba um pouco do que nossos alunos tem a dizer sobre nossas
+              aulas.
+            </p>
           </div>
-        </section>
-    )
+        </div>
+        <Carousel infiniteLoop autoPlay interval={4000} showStatus={false}>
+          <Card
+            name="Interesting Person"
+            description="Essa aula de inglês tem me ajudado imensamente. Minha pronunciação melhorou e consigo me comunicar mais facil e rapidamente."
+            source="https://picsum.photos/800/600"
+            altText="Placeholder Image"
+          />
+          <Card
+            name="Interesting Person 2"
+            description="Essa aula de inglês tem me ajudado imensamente. Minha pronunciação melhorou e consigo me comunicar mais facil e rapidamente."
+            source="https://picsum.photos/800/600"
+            altText="Placeholder Image"
+          />
+          <Card
+            name="Interesting Person 3"
+            description="Essa aula de inglês tem me ajudado imensamente. Minha pronunciação melhorou e consigo me comunicar mais facil e rapidamente."
+            source="https://picsum.photos/800/600"
+            altText="Placeholder Image"
+          />
+        </Carousel>
+      </div>
+    </section>
+  );
 }
