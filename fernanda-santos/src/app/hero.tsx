601b5312b1f5dd735abc65cf88cd3aa087ab50ee
Estevão <estevaog.gomes@gmail.com>
2023-04-28 12:49:41 -0300

    [Styles]Add styles for Hero section
    Adds image, styles, and new button for hero section. Changes button component to allow blue button

--- fernanda-santos/src/app/hero.tsx
+++ fernanda-santos/src/app/hero.tsx
@@ -1,8 +1,9 @@
 import { Open_Sans, PT_Sans, PT_Sans_Narrow } from "next/font/google";
 import Image from "next/image";
 
-import HeroImage from "@/assets/HeroImage3.png";
+import HeroImage from "@/assets/Hero.jpg";
 import blob from "@/assets/blob.svg";
+import { Button } from "@/components/button";
 
 const open_sans = Open_Sans({
   weight: "800",
@@ -34,9 +35,9 @@ export function Hero() {
     //   </div>
     //     <Image className="align-bottom object-cover -z-10" fill src={HeroImage} alt="Hero image" />
     // </div>
-    <div className="bg-slate-50 h-screen min-h-[75vh] lg:grid lg:grid-cols-2">
+    <div className="h-screen min-h-[75vh] overflow-hidden bg-white lg:grid lg:grid-cols-2">
       <div className="flex items-center px-4 text-center lg:col-span-1">
-        <div className="md:pr-12">
+        <div className="flex flex-col md:pl-24">
           <p
             className={`${pt_sans} border-1 mt-4 mb-4 rounded-md p-2 text-left md:text-2xl lg:text-5xl xl:text-6xl`}
           >
@@ -45,24 +46,32 @@ export function Hero() {
             aulas da
           </p>
           <h1
-            className={`${open_sans.className} rounded-lg border bg-brightred-500 p-2 text-2xl font-semibold text-white shadow-md lg:text-5xl xl:text-6xl`}
+            className={`${open_sans.className} rounded-lg border bg-brightred-500 p-2 text-2xl font-semibold text-white shadow-lg shadow-blue-900 lg:text-5xl xl:text-6xl`}
           >
-            Professora <p>Fernanda Santos</p>
+            Professora <p className="drop-shadow-lg">Fernanda Santos</p>
           </h1>
-          <h2 className="mt-4 text-lg lg:text-right lg:text-2xl">
+          <h2 className="mt-4 text-lg lg:max-w-[75%] lg:self-end lg:text-right lg:text-2xl">
             Aulas online particulares ou em grupo, no seu tempo, e com um preço
             que cabe no seu bolso!
           </h2>
+          <div className="mt-8 text-left">
+            <Button text="Entre em contato!" url="contact" blue />
+          </div>
         </div>
       </div>
-      <div className="justify-end self-center border-8 border-blue-900 rounded-full overflow-hidden h-[75%] relative m-16 mt-0 flex items-center lg:m-0 lg:h-5/6 lg:auto-cols-min lg:col-span-1">
-        <Image className="absolute -z-10" src={blob} alt="background blob" fill/>
-        <Image
-          className="object-contain"
-          fill
-          src={HeroImage}
-          alt="Hero image"
-        />
+      <div className="relative flex h-full items-center justify-end self-center lg:col-span-1 lg:auto-cols-min">
+        <div className="relative h-1/2 w-full translate-x-32">
+          <div className="absolute -z-10 h-full w-full translate-x-8 translate-y-8 bg-blue-900"></div>
+          <div className="absolute -z-10 h-full w-full -translate-x-8 -translate-y-8 bg-brightred-500"></div>
+          <Image
+            // className="object-contain"
+            fill
+            // width={1400}
+            // height={500}
+            src={HeroImage}
+            alt="Hero image"
+          />
+        </div>
       </div>
     </div>
   );
--- fernanda-santos/src/components/button.tsx
+++ fernanda-santos/src/components/button.tsx
@@ -1,11 +1,21 @@
 interface ButtonProps {
   url: string;
   text: string;
+  blue?: boolean;
 }
-export function Button({ url, text }: ButtonProps) {
+export function Button({ url, text, blue = false }: ButtonProps) {
+  if (blue) {
+    return (
+      <a href={`#${url}`}>
+        <button className="mx-auto rounded-lg border-4 border-transparent bg-blue-900 p-2 px-4 text-3xl font-bold text-white shadow-md hover:bg-opacity-80 hover:text-slate-200 hover:ring-2 hover:ring-brightred-500 hover:ring-offset-2">
+          {text}
+        </button>
+      </a>
+    );
+  }
   return (
     <a href={`#${url}`}>
-      <button className="p-2 mx-auto rounded-lg border-4 border-transparent bg-brightred-500 px-4 text-3xl shadow-md font-bold text-white hover:border-4 hover:border-blue-900 hover:bg-opacity-80 hover:text-slate-200">
+      <button className="mx-auto rounded-lg border-4 border-transparent bg-brightred-500 p-2 px-4 text-3xl font-bold text-white shadow-md hover:border-4 hover:border-blue-900 hover:bg-opacity-80 hover:text-slate-200">
         {text}
       </button>
     </a>
